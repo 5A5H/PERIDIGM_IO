f788597cc0f24b351323bfc0e65bc41e20531a24
Sascha Maassen <saschamaassen@uni-due.de>
2019-03-06 15:25:06 +0100

    Started with a Worksheet collected the info to use a material JC

---
+++ Worksheet.py
@@ -0,0 +1,23 @@
+# My Workspace
+
+#dictionary for correspondence material
+# "Density"                                     "double"    "7.83e-9"
+# "Young's Modulus"                             "double"    "2e5"
+# "Poisson's Ratio"                             "double"    "0.29"
+
+# "Constant A"                                  "double"    "792.0"
+# "Constant N"                                  "double"    "0.26"
+# "Constant B"                                  "double"    "510.0"
+# "Constant C"                                  "double"    "0.014"
+# "Constant M"                                  "double"    "1.03"
+
+# "Initial Material Temperature"                "double"    "298.0"
+# "Melting Temperature"                         "double"    "1793.0"
+# "Reference Temperature"                       "double"    "298.0"
+
+# "Reference Equivalent Plastic Strain Rate"    "double"    "1.0"
+# "Thermal Expansion Coefficient"               "double"    "3.2-5"
+# "Specific Heat Capacity"                      "double"    "383.0e6"
+
+# "Beta"                                        "double"    "0.9"
+# "Hourglass Stability Coefficient"             "double"    "0.005"
